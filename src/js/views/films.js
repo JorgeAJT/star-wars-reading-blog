5346df08591c493a450ef4562772d262ffc5f857
Jorge Jiménez <154094092+JorgeAJT@users.noreply.github.com>
2024-04-22 09:41:15 +0000

    favourite almost working and improved the design

--- src/js/views/films.js
+++ src/js/views/films.js
@@ -7,26 +7,23 @@ import { Card } from "../component/card";
 
 import "../../styles/home.css";
 
-export const Films = () => {
+export const Characters = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 	<div className="text-center mt-5">
-		<h1>Films</h1>
-		<div className="row d-flex mt-4">
-		{store.films.map((film, index) => 
-		<div className="col-12 col-md-6 col-xl-3 my-xl-2">
-			<Card 
-			key={index+1}
-            image={`https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/films/${index+1}.jpg`}
-			title={film.title}
-			/>
+		<h1>Characters</h1>
+		<div className="row d-flex">
+			{store.characters.map((character, index) => 
+			<div className="col-12 col-md-6 col-xl-3 my-xl-2">
+				<Card 
+				key={"c" + index+1}
+				image={`https://raw.githubusercontent.com/tbone849/star-wars-guide/master/build/assets/img/characters/${index+1}.jpg`}
+				title={character.name}
+				/>
+			</div>
+			)}
 		</div>
-		)}
-		</div>
-        <Link to="/">
-			<button className="btn btn-primary">Back home</button>
-		</Link>
 	</div>
 	);
 }
